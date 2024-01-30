5aecf9fa5751a5dffac123acdfe06c0a203792b1
Jian Xie <jianx@bu.edu>
2024-01-30 15:30:31 -0500

    cs 501 assignment 1

---
+++ HelloWorld/app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.helloworld
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun onClick(view: View) {
+        val display_text : TextView = findViewById(R.id.textView)
+        display_text.setText("Hello World!")
+    }
+}
